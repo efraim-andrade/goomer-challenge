374c6d52b80803e908ce488251b2ffcda6b57f8e
Efraim Andrade <efraim.dev@gmail.com>
2020-01-22 20:45:12 -0300

    feat(Structure): built the initial structure of the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GlobalStyles from '~/theme/global'
+
+import Routes from '~/routes';
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyles />
+
+      <Routes />
+    </>
+  );
+}
